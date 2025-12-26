28d7c55089e73cb12f221bad6b41bd688c4113d7
Dmitry1517 <94603546+Dmitry1517@users.noreply.github.com>
2025-12-26 07:05:25 +0000

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import GptCodex from './components/GptCodex.vue';
+
+</script>
+
+<template>
+  <div>
+    <GptCodex />
+  </div>
+</template>
+
+
---
+++ src/components/GptCodex.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+const apiKey = "sk-G5l4qD9dSDxiLsfpNyrhGCSjT6phqYZr";
+const baseURL = "https://api.proxyapi.ru/anthropic";
+
+const response = await fetch(`${baseURL}/v1/messages`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': apiKey,
+        'anthropic-version': '2023-06-01' // Важно указать версию API
+    },
+    body: JSON.stringify({
+        model: "claude-3-7-sonnet-20250219",
+        max_tokens: 1000,
+        messages: [
+            {
+                role: "user",
+                content: [
+                    {
+                        type: "text",
+                        text: "Какая у тебя модель?"
+                    }
+                ]
+            }
+        ]
+    })
+});
+
+const msg = await response.json();
+console.log(msg.content[0].text);
+
+
+</script>
+
+<template>
+    <h3>GPT-5.1-Codex-max</h3>
+</template>
